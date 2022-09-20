22f33f059ef525f934ff586fd60fb62ee1f9980c
Fahad Abbas <fahadabbas920@gmail.com>
2022-09-20 17:04:41 +0400

    chnaged basename to /

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename="https://fahadabbas920.github.io/portfolio/">
+    <BrowserRouter basename="/">
     <App />
     </BrowserRouter>
 );
