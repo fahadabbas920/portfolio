0f39ecd4bad345577c82d40ccc7df6c01093ee36
Fahad Abbas <fahadabbas920@gmail.com>
2022-09-20 18:26:17 +0400

    .....pathnames

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,25 +5,25 @@ const Navbar = () => {
     <nav className="navbar">
       <ul className="links-container">
         <li>
-          <Link to={"/"} className="link">
+          <Link to={"portfolio/"} className="link">
             <i className="fa-solid fa-house"></i>
             <span>Home</span>
           </Link>
         </li>
         <li>
-          <Link to={"/projects"} className="link">
+          <Link to={"portfolio/projects"} className="link">
             <i className="fa-solid fa-diagram-project"></i>
             <span>Projects</span>
           </Link>
         </li>
         <li>
-          <Link to={"/about"} className="link">
+          <Link to={"portfolio/about"} className="link">
             <i className="fa-solid fa-person"></i>
             <span>About</span>
           </Link>
         </li>
         <li>
-          <Link to={"/contact"} className="link">
+          <Link to={"portfolio/contact"} className="link">
             <i className="fa-solid fa-address-book"></i>
             <span>Contact</span>
           </Link>
