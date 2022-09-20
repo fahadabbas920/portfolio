f9054b47293a0125076b57aa10ba12e25ca2a1f6
Fahad Abbas <fahadabbas920@gmail.com>
2022-09-20 17:00:42 +0400

    added basename in browser router

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename="https://fahadabbas920.github.io/portfolio/">
     <App />
     </BrowserRouter>
 );
