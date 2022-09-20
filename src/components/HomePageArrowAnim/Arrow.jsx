372e83aae8fc72525e3fed88793f50a81fc63a63
Fahad Abbas <fahadabbas920@gmail.com>
2022-09-20 16:57:50 +0400

    Updates

--- src/components/HomePageArrowAnim/Arrow.jsx
+++
@@ -1,9 +0,0 @@
-import  classes  from "./Arrow.module.css";
-
-const Arrow = () => {
-    return ( 
-        <div className={classes.down_arrow}></div>
-     );
-}
- 
-export default Arrow;
--- src/components/footer/footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="footer">
-      &copy; Fahad Abbas 2022 <i className="fa-solid fa-heart heart"></i>
-    </footer>
-  );
-};
-
-export default Footer;
